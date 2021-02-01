e8301b77d88c9ef27ff198c35cdb749c7d79144d
ZhengYiwei1124 <1812673400@qq.com>
2021-02-01 16:42:20 +0800

    update function automatically reporting with Github Actions

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
+import sys
+from class_define import User
+from class_define import Email
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from class_define import User
-import send_email
-import accounts
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
 
 # 实例化一个user，并定义用户名和密码
-user = User(accounts.daka_uid, accounts.daka_pwd)
+user = User(sys.argv[1], sys.argv[2])
+# 实例化一个email，并设置发送邮箱，邮箱密码，接收邮箱
+email = Email(sys.argv[3], sys.argv[4], sys.argv[5])
+
+
+# 定义发送邮件函数
+def send(content):
+    try:
+        # 连接163邮箱的服务器并登录邮箱
+        con = smtplib.SMTP_SSL('smtp.163.com', 465)
+        con.login(email.mail_uid, email.mail_pwd)
+
+        # 创建邮件对象
+        msg = MIMEMultipart()
+
+        # 设置邮件主题、发件人、收件人、邮件内容
+        msg['Subject'] = Header('找回密码', 'utf-8').encode()
+        msg['From'] = email.mail_from
+        msg['To'] = email.mail_to
+        text = MIMEText(content, 'plain', 'utf-8')
+        msg.attach(text)
+
+        # 发送邮件后退出邮箱
+        con.sendmail(email.mail_from, email.mail_to, msg.as_string())
+        con.quit()
+
+        print("提醒邮件发送成功！")
+
+    except smtplib.SMTPException as e:
+        print('提醒邮件发送失败！error：', e)
+
 
 # 设置不显示浏览器窗口
 chrome_options = Options()
@@ -53,4 +87,4 @@ except Exception as err:
 
 finally:
     browser.quit()
-    send_email.send(email_message)
+    send(email_message)
